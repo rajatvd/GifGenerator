73acd7036c30265c5e3e7d1387442a79385345f3
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-02-09 00:10:25 +0530

    added script

---
+++ gif_generation_service.py
@@ -0,0 +1,52 @@
+"""Generate and send gifs at time intervals."""
+from functools import partial
+
+from sacred import Experiment
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from gif_generators import GIFFERS, CONFIGS
+from telegram_helpers import make_telegram_bot, send_gif
+
+ex = Experiment("gif_generation")
+
+
+@ex.config
+def main_config():
+    """Make main config."""
+    giffer = 'neural_ode'
+    ex.add_config({giffer: CONFIGS[giffer]})
+
+    infofile = 'info.json'
+
+    hours = 1
+    minutes = 0
+    seconds = 0
+
+    count = 2
+
+
+@ex.capture
+def make_and_send_gif(giffer, bot, id, _config, _log):
+    """Make gif using the giffer with the config.
+
+    Send it using the bot to the given id.
+    """
+    gif = GIFFERS[giffer](**_config[giffer], _log=_log)
+    send_gif(gif, bot, id)
+
+
+def job(infofile, count):
+    bot, id = make_telegram_bot(infofile)
+    for i in range(count):
+        make_and_send_gif(bot=bot, id=id)
+
+
+@ex.automain
+def main(giffer, infofile,
+         hours, minutes, seconds,
+         _config, _log):
+
+    sched = BlockingScheduler()
+    sched.add_job(partial(job, infofile, count), 'interval',
+                  hours=hours, minutes=minutes, seconds=seconds)
+    sched.start()
